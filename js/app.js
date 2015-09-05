4ec7c8a3c62763a57742493435072b7c273b40b3
Jason Cho <dimhaku@gmail.com>
2015-09-04 23:51:15 -0700

    fix

--- js/app.js
+++ js/app.js
@@ -1,115 +1,92 @@
-(function () {
-   'use strict';
-   // this function is strict...
-}());
-$(document).ready(function() {
 
-//User contructor
-var Tracker = function(){
-  this.isNewBattle = true;
+'use strict'
+
+var Photo = function(fileLocation, startVotes) {
+  this.path  = fileLocation;
+  this.votes = startVotes;
 };
-var Photo = function(path) { //constructor
-  this.path = path;
-  this.img        = document.createElement('img');
-  this.img.src    = this.path;
-  this.votes = 0;
-   $(this.img).on('click', function(e){
-    if(tracker.isNewBattle){
-      e.target.votes++;
-      $(e.target).toggleClass('winner');
-      tracker.isNewBattle = false;
-      var targetVote = e.target.votes;
-      var otherVote = 0;
-      if(e.target.parentElement.id === 'imgL'){
-        otherVote = document.getElementById('imgR').children[0].votes;
-        chart(targetVote, otherVote);
-      } else {
-        otherVote = document.getElementById('imgL').children[0].votes;
-        chart(otherVote, targetVote);
-      }
+
+var Tracker = function() {
+
+  this.photos = [];
+
+  this.maxPhotos = 14;
+
+  this.leftPhoto = null;
+
+  this.rightPhoto = null;
+
+  $('#left').on('click', $.proxy(this.leftClicked, this));
+  $('#right').on('click', $.proxy(this.rightClicked, this));
+  $('#next').on('click', $.proxy(this.showNextPair, this));
+};
+
+Tracker.prototype.initializeLocalData = function() {
+  for(var i = 0; i < this.maxPhotos; i++) {
+    var photo = new Photo('img/kitten/kit' + (i + 1) + '.jpg', 0);
+    this.photos.push(photo);
+
+  }
+};
+
+Tracker.prototype.initializeImgurData = function() {
+  $.ajax ( {
+    url: 'https://api.imgur.com/3/album/DDoWy',
+    method: 'GET',
+    headers: {
+      'Authorization': 'Client-ID 6372bb21c05054d'
     }
+  })
+  .done($.proxy(function(res) {
+    for (var i = 0; i < res.data.images.length; i++) {
+      var photo = new Photo(res.data.images[i].link, 0);
+      this.photos.push(photo);
+    }
+    this.showNextPair();
+  }, this))
+  .fail(function(err) {
+    console.log(err);
   });
 };
 
 
-var alex = new Photo('img/kitten_pictures/Alex.jpg');
-var bob = new Photo('img/kitten_pictures/Bob.jpg');
-var boots = new Photo('img/kitten_pictures/Boots.jpg');
-var felica = new Photo('img/kitten_pictures/Felica.jpg');
-var frank = new Photo('img/kitten_pictures/Frank.jpg');
-var fred = new Photo('img/kitten_pictures/Fred.jpg');
-var jack = new Photo('img/kitten_pictures/Jack.jpg');
-var jill = new Photo('img/kitten_pictures/Jill.jpg');
-var kasey = new Photo('img/kitten_pictures/Kasey.jpg');
-var kora = new Photo('img/kitten_pictures/Kora.jpg');
-var molly = new Photo('img/kitten_pictures/Molly.jpg');
-var steve = new Photo('img/kitten_pictures/Steve.jpg');
-var tigger = new Photo('img/kitten_pictures/Tigger.jpg');
-var todd = new Photo('img/kitten_pictures/Todd.jpg');
+Tracker.prototype.leftClicked = function() {
+  $('#left').addClass('highlight');
+  this.leftPhoto.votes++;
 
-var photoArray = [alex, bob, boots, felica, frank, fred, jack, jill, kasey, kora, molly, steve, tigger, todd];
-
-Tracker.prototype.getRandomInt = function() {
-
-  return Math.floor((Math.random() * 14) + 0);
-  //generate a random number to select an image from photoArray
-
-};
-
-var battleCats = new Tracker ();
-var leftCats = photoArray[battleCats.getRandomInt()].path;
-var rightCats = photoArray[battleCats.getRandomInt()].path;
-
-while (leftCats === rightCats) {
-  rightCats = photoArray[battleCats.getRandomInt()].path;
-  leftCats = photoArray[battleCats.getRandomInt()].path;
 }
 
-console.dir (leftCats);
-console.dir (rightCats);
+Tracker.prototype.rightClicked = function() {
+  $('#right').addClass('highlight');
+  this.rightPhoto.votes++;
 
-Tracker.prototype.displayPhotos = function() {
-var first=document.getElementById('left');
-var leftPhoto = document.createElement('img');
-leftPhoto.src=leftCats;
-first.appendChild(leftPhoto);
+}
 
-var second=document.getElementById('right');
-var rightPhoto = document.createElement('img');
-rightPhoto.src=rightCats;
-second.appendChild(rightPhoto);
+// Return: random index into photos array
+Tracker.prototype.generateRandom = function() {
+  return Math.floor(Math.random() * this.photos.length);
 };
 
-battleCats.displayPhotos();
-
-document.getElementById('left').addEventListener('click', function (e) {
-        e.target.style.outline = "solid blue 5px";
-});
-
-document.getElementById('right').addEventListener('click', function (e) {
-        e.target.style.outline = "solid red 5px";
-});
-
-var pieData = [
-  {
-    value: 20,
-    color:"#878BB6"
-  },
-  {
-    value : 40,
-    color : "#4ACAB4"
-  },
-];
-var pieOptions = {
-  segmentShowStroke : false,
-  animateScale : true
+// Return random photo object from the photo array
+Tracker.prototype.randomPhoto = function() {
+  return this.photos[this.generateRandom()];
 };
 
-var countries= document.getElementById("countries").getContext("2d");
-new Chart(countries).Pie(pieData, pieOptions);
-Tracker.prototype.displayWinner = function() {
-
+Tracker.prototype.showNextPair = function() {
+  this.leftPhoto = this.randomPhoto();
+  this.rightPhoto = this.randomPhoto();
+  while (this.leftPhoto === this.rightPhoto) {
+    this.rightPhoto = this.randomPhoto();
+  }
+  $('#left').attr('src', this.leftPhoto.path);
+  $('#right').attr('src', this.rightPhoto.path);
+  $('#left').removeClass('highlight');
+  $('#right').removeClass('highlight');
 };
+
+
+$(document).ready(function() {
+
+  var tracker = new Tracker();
+  tracker.initializeImgurData();
 });
-
-
